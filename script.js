8034e04e265fd8b4c5db3a8211b2059b39b7a644
Aqsa Zaboor <aqsazaboor786@gmail.com>
2023-05-17 22:56:48 +0500

    function

--- script.js
+++ script.js
@@ -103,3 +103,12 @@ function func() {
   console.log(arr.pop());
 }
 func();
+function myFunc(theArr) {
+  theArr[0] = 30;
+}
+
+const arr = [45];
+
+console.log(arr[0]); // 45
+myFunc(arr);
+console.log(arr[0]); // 30
