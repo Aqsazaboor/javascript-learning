b0d13a6cae041cbecf83080fe8dfb45b9e545fb5
Aqsa Zaboor <aqsazaboor786@gmail.com>
2023-05-25 13:11:01 +0500

    statement and expression ,ternay opertor

---
+++ statment.js
@@ -0,0 +1,12 @@
+// 3+4 is statement
+
+3 + 4;
+console.log(3 + 4);
+// the answer  is expression
+
+// ternary operator
+const age = 44;
+// age >= 50 ? console.log("i am drink 🥛") : console.log("i am drink water");
+
+const drink = age >= 12 ? " drink🍹" : " milk🥛";
+console.log(drink);
